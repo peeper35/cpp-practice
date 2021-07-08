b1bf09ef38ffa8e0a8ba9cc8ac4a2d7540fcd30c
Sahil Tembhare <isr.sahil.mk@gmail.com>
2021-07-08 12:51:36 +0530

    prime number thingy

--- prime_numbers/check_till_number.cpp
+++ prime_numbers/check_till_number.cpp
@@ -3,27 +3,5 @@
 using namespace std;
 
 int main(){
-	long number;
-	bool flag = true;
-
-	cout << "Enter a number: ";
-	cin >> number;
-
-	for(int j = 1; j <= number; j++ ){
-		bool flag = true;
-
-		for(int i = 2; i < j/2; i++){
-			if(j%i == 0){
-				flag = false;
-				break;
-			}
-		}
-
-		if(flag){
-			cout << j <<" Prime Number" << endl;
-		}
-		else{
-			cout << j << " Not Prime Number" << endl;
-		}
-	}
+	
 }
